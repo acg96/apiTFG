0507236047d1ad9f4dd7c036320c70312702feb4
Andres <UO244499@uniovi.es>
2019-07-30 16:51:14 +0200

    Fixed some bugs

--- routes/rstudentapi.js
+++ routes/rstudentapi.js
@@ -3,15 +3,16 @@ module.exports = function (app, bdManagement, logger) {
         var username= res.user;
         var urlEncoded= req.query.url_;
         var urlDecoded= decodeURIComponent(urlEncoded);
-        logger.info(urlDecoded); //TODO
         var response= {result: false};
         if (username === "UO111111") {
             response.result= true;
         }
         if (response.result) {
+            logger.info("Access granted to user " + res.user + ". URL: " + urlDecoded + " - IP: " + req.ip);
             res.status(200);
             res.json({access: true, message: 'Access granted', privileges: true});
         } else {
+            logger.info("Access denied to user " + res.user + ". URL: " + urlDecoded + " - IP: " + req.ip);
             res.status(403);
             res.json({access: true, message: 'Access denied', privileges: false});
         }
