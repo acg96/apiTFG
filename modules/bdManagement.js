3b8466873897b8633d5c834293caf888fc912c4e
Andres <UO244499@uniovi.es>
2019-12-27 12:02:41 +0100

    Now it duplicates collections that not change on backups to allow restoring to a concrete point in the past

--- modules/bdManagement.js
+++ modules/bdManagement.js
@@ -42,14 +42,88 @@ module.exports = {
             }
         });
     },
+    duplicateCollection: function(collectionsInfo, timeMilliseconds, index, mongo, errTime, anyErr, resultRenaming, callback){
+        const oldName= collectionsInfo[index];
+        const newName = oldName + timeMilliseconds;
+        this.getValuesFromCollection({}, oldName, collectionValues => {
+            if (collectionValues != null){
+                this.addSomeValuesToCollection(collectionValues, newName, countOfAdditions => {
+                    if (countOfAdditions == null){
+                        if (errTime < 8) {
+                            this.duplicateCollection(collectionsInfo, timeMilliseconds, index, mongo, ++errTime, anyErr, resultRenaming, callback);
+                        } else{
+                            if (index === collectionsInfo.length - 1){
+                                callback(false);
+                            } else {
+                                this.duplicateCollection(collectionsInfo, timeMilliseconds, ++index, mongo, 0, 1, resultRenaming, callback);
+                            }
+                        }
+                    } else{
+                        if (index === collectionsInfo.length - 1){
+                            callback(anyErr === 0 && resultRenaming);
+                        } else{
+                            this.duplicateCollection(collectionsInfo, timeMilliseconds, ++index, mongo, 0, anyErr, resultRenaming, callback);
+                        }
+                    }
+                });
+            } else{
+                if (errTime < 8) {
+                    this.duplicateCollection(collectionsInfo, timeMilliseconds, index, mongo, ++errTime, anyErr, resultRenaming, callback);
+                } else{
+                    if (index === collectionsInfo.length - 1){
+                        callback(false);
+                    } else {
+                        this.duplicateCollection(collectionsInfo, timeMilliseconds, ++index, mongo, 0, 1, resultRenaming, callback);
+                    }
+                }
+            }
+        });
+    },
     renameAllCollections: function(callback){
         const collectionNames= ["users", "groups", "notifications", "modules", "slots"];
         this.renameCertainCollections(collectionNames, resultRemaining => {
             callback(resultRemaining);
         });
     },
+    getValuesFromCollection: function (criteria, collectionName, callbackFunction) {
+        const mongo = this.getMongoClientObject();
+        mongo.connect(function(err) {
+            if (err) {
+                callbackFunction(null);
+            } else {
+                const collection = mongo.db(this.app.get('dbName')).collection(collectionName);
+                collection.find(criteria).toArray(function (err, collectionValues) {
+                    if (err) {
+                        callbackFunction(null);
+                    } else {
+                        callbackFunction(collectionValues);
+                    }
+                    mongo.close();
+                }.bind(this));
+            }
+        }.bind(this));
+    },
+    addSomeValuesToCollection: function (collectionValues, collectionName, callbackFunction) {
+        const mongo = this.getMongoClientObject();
+        mongo.connect(function(err) {
+            if (err) {
+                callbackFunction(null);
+            } else {
+                const collection = mongo.db(this.app.get('dbName')).collection(collectionName);
+                collection.insertMany(collectionValues, function (err, result) {
+                    if (err) {
+                        callbackFunction(null);
+                    } else {
+                        callbackFunction(result.insertedCount);
+                    }
+                    mongo.close();
+                }.bind(this));
+            }
+        }.bind(this));
+    },
     renameCertainCollections: function(collectionNames, callback){
         let mongo = this.getMongoClientObject();
+        const allCollections= ["users", "groups", "notifications", "modules", "slots"];
         mongo.connect(function(err) {
             if (err){
                 callback(false);
@@ -60,15 +134,29 @@ module.exports = {
                     } else{
                         const timeMilliseconds= this.app.get('currentTimeWithSeconds')().valueOf().toString();
                         const collectionsToChange= [];
+                        const collectionsToDuplicate= []; //Used to copy the collections that not change to allow backup recovery
                         for (let i= 0; i < collectionsInfo.length; ++i){
                             if (collectionNames.includes(collectionsInfo[i].name) && !collectionsToChange.includes(collectionsInfo[i].name)){
                                 collectionsToChange.push(collectionsInfo[i].name);
+                            } else if (!collectionNames.includes(collectionsInfo[i].name) && allCollections.includes(collectionsInfo[i].name) && !collectionsToDuplicate.includes(collectionsInfo[i].name)) {
+                                collectionsToDuplicate.push(collectionsInfo[i].name);
                             }
                         }
-                        this.renameCollection(collectionsToChange, timeMilliseconds, 0, mongo, 0, 0, resultRenaming => {
-                            callback(resultRenaming);
-                            mongo.close();
-                        });
+                        if (collectionsToChange.length > 0) {
+                            this.renameCollection(collectionsToChange, timeMilliseconds, 0, mongo, 0, 0, resultRenaming => {
+                                if (collectionsToDuplicate.length > 0) {
+                                    this.duplicateCollection(collectionsToDuplicate, timeMilliseconds, 0, mongo, 0, 0, resultRenaming, resultDuplicateProcess => {
+                                        callback(resultDuplicateProcess);
+                                        mongo.close();
+                                    });
+                                } else{
+                                    callback(resultRenaming);
+                                    mongo.close();
+                                }
+                            });
+                        } else{
+                            callback(true);
+                        }
                     }
                 });
             }
